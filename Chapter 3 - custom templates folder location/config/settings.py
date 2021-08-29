41b1e3f2239a8aa1d5c05ea6eb068d2edccddfeb
KubaTaba1uga <jakub@buczynski.net.pl>
2021-08-29 15:38:15 +0200

    Chapter 3 - custom templates structure

--- Chapter 3 - custom templates folder location/config/settings.py
+++ Chapter 3 - custom templates folder location/config/settings.py
@@ -31,12 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.admin', 'django.contrib.auth',
+    'django.contrib.contenttypes', 'django.contrib.sessions',
+    'django.contrib.messages', 'django.contrib.staticfiles', 'pages'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +51,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ Chapter 3 - custom templates folder location/pages/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+
+from .views import hello_world
+
+urlpatterns = [path('hello_world', hello_world.as_view(), name='hello_world')]

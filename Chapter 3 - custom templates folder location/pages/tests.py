22414967369c1fd146830ca78857a096b25612f0
KubaTaba1uga <jakub@buczynski.net.pl>
2021-08-29 17:31:55 +0200

    Chapter 3 - tests

--- Chapter 3 - custom templates folder location/pages/tests.py
+++ Chapter 3 - custom templates folder location/pages/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 
-# Create your tests here.
+
+class Response200(SimpleTestCase):
+    """
+    Ensures that pages which should be returning 200, are doing it 
+    """
+    def test_hello_word_page_status_code(self):
+        response = self.client.get('/hello_world')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_me_page_status_code(self):
+        response = self.client.get('/about_me')
+        self.assertEqual(response.status_code, 200)

46bcfbd0d2d157f91ae954cffd1a848a2fdabcaa
KubaTaba1uga <jakub@buczynski.net.pl>
2021-08-30 15:09:06 +0200

    Chapter 4 - 'Django administration' portal

--- Chapter 4/posts/models.py
+++ Chapter 4/posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    content = models.TextField()
+
+    def __str__(self) -> str:
+        """
+        represent Post instance preview in 'Django administration' portal
+        """
+        return self.content[:50]

11abe3f77c2896a8bb4134370d52f604d8411da2
KubaTaba1uga <jakub@buczynski.net.pl>
2021-09-14 18:04:58 +0200

    Chapter 5 - 'Post model' details view

--- Chapter 5/blog/urls.py
+++ Chapter 5/blog/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
-from .views import HomePage
+from .views import HomePage, PostDetails
 
-urlpatterns = [path('', HomePage.as_view(), name='home_page')]
+urlpatterns = [
+    path('', HomePage.as_view(), name='home_page'),
+    path('post/<int:pk>', PostDetails.as_view(), name='')
+]
+"""
+primary key (id) of bounded model is passed to View as integer, if value preceded with /post
+"""
--- Chapter 5/blog/views.py
+++ Chapter 5/blog/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
 from .models import Post
 
@@ -6,3 +6,18 @@ from .models import Post
 class HomePage(ListView):
     template_name = "index.html"
     model = Post
+
+
+class PostDetails(DetailView):
+    template_name = "post.html"
+
+    model = Post
+    """
+    DetailView provide context object wich can be used in the template
+    by 'post' or 'object' name
+    """
+
+    # context_object_name = "the_post"
+    """
+    Provide alternative name for context object
+    """

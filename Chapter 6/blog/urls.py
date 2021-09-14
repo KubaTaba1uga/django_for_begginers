8124b8728ab25e99c9f0989e7526339ef7117345
KubaTaba1uga <jakub@buczynski.net.pl>
2021-09-14 20:55:37 +0200

    Chapter 6 - styling

--- Chapter 6/blog/urls.py
+++ Chapter 6/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import HomePage, PostCreate, PostDetails
 
 urlpatterns = [
-    path('', HomePage.as_view(), name='home_page'),
+    path('', HomePage.as_view(), name='home'),
     path('post/<int:pk>', PostDetails.as_view(), name='post'),
     path('post/new', PostCreate.as_view(), name='new_post')
 ]

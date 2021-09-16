359ed55e17378a9911c167bf5b7257a12ed519d4
KubaTaba1uga <jakub@buczynski.net.pl>
2021-09-16 23:42:58 +0200

    Chapter 7 - log in

--- Chapter 7/config/settings.py
+++ Chapter 7/config/settings.py
@@ -122,3 +122,8 @@ Tell Django to look for static files in newly created folder
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'post_show_all'
+"""
+Specify where to redirect user, after succesfull log in
+"""
--- Chapter 7/config/urls.py
+++ Chapter 7/config/urls.py
@@ -16,4 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path('admin/', admin.site.urls), path('', include('blog.urls'))]
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
+]
+"""
+    Add log-in & log-out functionalities, under '<app_address>/accounts' url
+
+"""

faba4add4476f6136ab3449569f66cbf53704d83
KubaTaba1uga <jakub@buczynski.net.pl>
2021-10-08 21:19:52 +0200

    Chapter 15 - `CommentModel`

--- Chapter 15/articles/models.py
+++ Chapter 15/articles/models.py
@@ -9,13 +9,28 @@ class ArticleModel(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(
-        get_user_model(),
-        #  Tell Django to use `CustomUser` as foreign key
-        on_delete=models.CASCADE)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse("article_detail_url", kwargs={"pk": self.id})
+
+
+class CommentModel(models.Model):
+    body = models.TextField()
+    author = models.ForeignKey(
+        get_user_model(),
+        #  Tell Django to use `CustomUser` as foreign key
+        on_delete=models.DO_NOTHING)
+    article = models.ForeignKey(
+        ArticleModel,
+        # Tell Django to use `ArticleModel` as foreign key
+        on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.body[:10]
+
+    def get_absolute_url(self):
+        return reverse("article_detail_url", kwargs={"pk": self.article.id})

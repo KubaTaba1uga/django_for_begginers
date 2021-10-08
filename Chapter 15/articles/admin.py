811e5da0415b348d79d0e8a05cd6d5dad743c042
KubaTaba1uga <jakub@buczynski.net.pl>
2021-10-08 21:30:51 +0200

    Chapter  15 - `CommentModel` stacked inline admin

--- Chapter 15/articles/admin.py
+++ Chapter 15/articles/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
 from .models import ArticleModel, CommentModel
 
-admin.site.register(ArticleModel)
+
+class CommentInline(admin.StackedInline):
+    """Show `CommentModel` on related 
+        objects, by foreign key
+    """
+    model = CommentModel
+    extra = 0
+    """Show only exsisting objects
+    """
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    """Add capability to admin portal 
+    """
+    inlines = [
+        CommentInline,
+    ]
+
+
+admin.site.register(ArticleModel, ArticleAdmin)
+"""Register capability to ArticleModel
+"""
 admin.site.register(CommentModel)

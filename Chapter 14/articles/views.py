8c9346a4b849b5fa3bf90178e26286db385734d5
KubaTaba1uga <jakub@buczynski.net.pl>
2021-10-08 17:14:47 +0200

    Chapter 14 - require log-in to access website

--- Chapter 14/articles/views.py
+++ Chapter 14/articles/views.py
@@ -1,38 +1,53 @@
 from django.urls import reverse_lazy
 from django.views.generic import (DetailView, ListView, DeleteView, UpdateView,
                                   CreateView)
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 # Custom Code
 from .models import ArticleModel
 
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     template_name = 'article_list_page.html'
     model = ArticleModel
     context_object_name = 'article_list'
 
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin, DetailView):
     template_name = 'article_detail_page.html'
     model = ArticleModel
     context_object_name = 'article'
 
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin, DeleteView):
     template_name = 'article_delete_page.html'
     model = ArticleModel
     success_url = reverse_lazy('article_list_url')
     context_object_name = 'article'
 
 
-class ArticleEditView(UpdateView):
+class ArticleEditView(LoginRequiredMixin, UpdateView):
     template_name = 'article_edit_page.html'
     model = ArticleModel
     fields = ('title', 'body')
     context_object_name = 'article'
 
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     template_name = 'article_create_page.html'
     model = ArticleModel
-    fields = ('title', 'body', 'author')
+    fields = ('title', 'body')
     context_object_name = 'article'
+
+    def form_valid(self, form):
+        """ Overwrite form validation method
+        
+                Code flow:
+                    0. Pass `Form` as method argument
+                    1. Set up current logged in user, as `Form`'s author
+                    2. Revoke not overwriten form validation method,
+                        passing `Form` as method argument
+                         
+        """
+        form.instance.author = self.request.user
+        return super().form_valid(form)
